72f80bf6f095b335ac4bc647f357aaf88daa0e0d
EndIsNear <alexanderdespotov@gmail.com>
2015-12-20 19:01:02 +0200

    Add simple vec2 class

---
+++ NN-aim/Vec2.h
@@ -0,0 +1,43 @@
+#pragma once
+
+struct Vector2
+{
+	float x, y;
+	Vector2() : x(0.f), y(0.f) {}
+	Vector2(const float _x, const float _y) : x(_x), y(_y) {}
+
+	inline int getIntX()
+	{
+		return static_cast<int>(x + 0.5f);
+	}
+
+	inline int getIntY()
+	{
+		return static_cast<int>(y + 0.5f);
+	}
+
+	inline Vector2 operator+(const Vector2& r)
+	{
+		return Vector2(x + r.x, y + r.y);
+	}
+
+	inline Vector2 operator-(const Vector2& r)
+	{
+		return Vector2(x - r.x, y - r.y);
+	}
+
+	inline Vector2& operator+=(const Vector2& r)
+	{
+		x += r.x;
+		y += r.y;
+		return *this;
+	}
+
+	inline Vector2& operator-=(const Vector2& r)
+	{
+		x -= r.x;
+		y -= r.y;
+		return *this;
+	}
+
+};
