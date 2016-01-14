dcf2e1a264a3c32a08c66f3a98042ae6999a5a4d
EndIsNear <alexanderdespotov@gmail.com>
2016-01-14 22:54:02 +0200

    Clear warnings

--- NN-aim/utils.cpp
+++ NN-aim/utils.cpp
@@ -16,10 +16,10 @@ void fill_circle(SDL_Surface *surface, int cx, int cy, int radius, Uint32 pixel)
 	for (double dy = 1; dy <= r; dy += 1.0)
 	{
 		double dx = floor(sqrt((2.0 * r * dy) - (dy * dy)));
-		int x = cx - dx;
+		int x = static_cast<int>(cx - dx);
 
-		Uint8 *target_pixel_a = (Uint8 *)surface->pixels + ((int)(cy + r - dy)) * surface->pitch + x * BPP;
-		Uint8 *target_pixel_b = (Uint8 *)surface->pixels + ((int)(cy - r + dy)) * surface->pitch + x * BPP;
+		Uint8 *target_pixel_a = (Uint8 *)surface->pixels + (static_cast<int>(cy + r - dy)) * surface->pitch + x * BPP;
+		Uint8 *target_pixel_b = (Uint8 *)surface->pixels + (static_cast<int>(cy - r + dy)) * surface->pitch + x * BPP;
 
 		for (; x <= cx + dx; x++)
 		{
@@ -35,7 +35,7 @@ SDL_Surface *  drawCircle(const float radius, const char red, const char green, 
 {
 	const int size = static_cast<int>((radius + 0.5f) * 2);
 	SDL_Surface * result = SDL_CreateRGBSurface(0, size, size, 32, 0, 0, 0, 0);
-	fill_circle(result, size / 2, size / 2, radius, SDL_MapRGB(result->format, red, green, blue));
+	fill_circle(result, size / 2, size / 2, size/2, SDL_MapRGB(result->format, red, green, blue));
 
 	return result;
 }
