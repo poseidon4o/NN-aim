67668792a14727932cca518b007a1a721dffa0bb
stoyanh <stoqnhristo93@gmail.com>
2016-01-29 19:08:30 +0200

    neural network basic

---
+++ NN-aim/NeuralNetwork.cpp
@@ -0,0 +1,46 @@
+#include "NeuralNetwork.h"
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+Neuron::Neuron(int inputs): m_inputs(inputs)
+{
+	srand(time(NULL));
+
+	float a = 0.f, b = 30.f;
+
+	for(int i = 0; i < m_inputs + 1; ++i) // plus 1 for the bias
+	{
+		float value = (b - a) * (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) + a;
+		m_weights.push_back(value);
+	}
+}
+
+NeuronLayer::NeuronLayer(int numNeurons, int inputsPerNeuron)
+{
+	m_numNeurons = numNeurons;
+	for(int i = 0; i < m_numNeurons; ++i)
+	{
+		m_neurons.push_back(Neuron(inputsPerNeuron));
+	}
+}
+
+NeuralNetwork::NeuralNetwork(): m_firstLayer(neuronsPerLayer, 4),
+		m_secondLayer(neuronsPerLayer, 5)
+{
+
+}
+
+float NeuralNetwork::sigmoid(float x) const
+{
+	return 1.f / (1.f + exp(x));
+}
+
+Move NeuralNetwork::calculateMove(bool inFov, bool bulletInFov, bool alreadyFired, float fov)
+{
+	Move move;
+
+
+
+	return move;
+}
---
+++ NN-aim/NeuralNetwork.h
@@ -0,0 +1,47 @@
+#ifndef NEURALNETWORK_H_
+#define NEURALNETWORK_H_
+
+#include <vector>
+
+struct Move
+{
+	bool advanceStraight;
+	bool turnLeft;
+	bool turnRight;
+	bool shoot;
+	float newFov;
+};
+
+struct Neuron
+{
+	Neuron(int inputs);
+
+	int m_inputs;
+	std::vector<float> m_weights;
+};
+
+struct NeuronLayer
+{
+	NeuronLayer(int numNeurons, int inputsPerNeuron);
+
+	int m_numNeurons;
+	std::vector<Neuron> m_neurons;
+};
+
+class NeuralNetwork
+{
+public:
+	NeuralNetwork();
+	Move calculateMove(bool inFov, bool bulletInFov, bool alreadyFired, float fov);
+private:
+	float sigmoid(float x) const;
+
+	static const int neuronsPerLayer = 5;
+
+	NeuronLayer m_firstLayer;
+	NeuronLayer m_secondLayer;
+};
+
+
+
+#endif /* NEURALNETWORK_H_ */
