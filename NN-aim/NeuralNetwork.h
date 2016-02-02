02ccb5f4c71223b51a6e986cfc803f0e5edde37b
stoyanh <stoqnhristov93@gmail.com>
2016-02-02 16:55:17 +0200

    Removed input canShoot and output turnRight

--- NN-aim/NeuralNetwork.h
+++ NN-aim/NeuralNetwork.h
@@ -43,8 +43,8 @@ private:
 	inline bool castToBool(float activation) const;
 
 	static const int neuronsPerLayer = 5;
-	static const int layer1InputsPerNeuron = 4;
-	static const int layer2InputsPerNeuron = 5;
+	static const int layer1InputsPerNeuron = 3;
+	static const int layer2InputsPerNeuron = 4;
 	static const int layersCount = 2;
 
 	NeuronLayer m_layers[layersCount];
